222cffb8becf330714c72451e1bb93aa5aeae0ad
Markus Montonen <montonen.markus@gmail.com>
2022-12-19 12:38:06 +0200

    Initial commit

---
+++ android/src/main/java/fi/scandium/capliveactivity/LiveActivity.java
@@ -0,0 +1,11 @@
+package fi.scandium.capliveactivity;
+
+import android.util.Log;
+
+public class LiveActivity {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
